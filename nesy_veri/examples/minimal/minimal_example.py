02a1555a7e3b154905d36478a21fc1eeaa96354b
yuzer <bmanginas@gmail.com>
2024-11-22 13:09:17 +0200

    update minimal example for ICL

--- nesy_veri/examples/minimal/minimal_example.py
+++ nesy_veri/examples/minimal/minimal_example.py
@@ -1,12 +1,7 @@
 import torch
-import operator
 import numpy as np
-from torch import nn
 from pathlib import Path
-from pysdd.sdd import SddNode
-from torchvision import transforms
-from torch.utils.data import DataLoader
-from torchvision.datasets import MNIST
+from rich.progress import track
 from auto_LiRPA import (
     BoundedModule,
     BoundedTensor,
@@ -14,99 +9,112 @@ from auto_LiRPA import (
     register_custom_op,
 )
 
-from nesy_veri.utils import sdd_parse_native, eval_sdd
-from nesy_veri.examples.mnist_addition.network_training import (
-    get_mnist_network,
+from nesy_veri.utils import NetworksPlusCircuit, example_is_robust
+from nesy_veri.custom_ops import CustomBoundSoftmax, CustomConcat
+from nesy_veri.examples.mnist_addition.network_training import get_mnist_network
+from nesy_veri.examples.mnist_addition.mnist_utils import (
+    MultiDigitAdditionDataset,
+    get_correctly_classified_examples,
 )
 
 
-def get_test_circuit():
-    # fmt: off
-    from pysdd.sdd import SddManager
-    manager = SddManager(20, 0)
-    (
-        x0, x1, x2, x3, x4, x5, x6, x7, x8, x9, 
-    ) = [manager.literal(i) for i in range(1, 11)]
+def get_bounded_modules_and_samples_to_verify(
+    softmax: bool, num_digits: int, test_dataset: MultiDigitAdditionDataset
+):
+    model_path = (
+        Path(__file__).parent.parent
+        / f"mnist_addition/checkpoints/trained_model{'_softmax' if softmax else ''}.pth"
+    )
+    mnist_cnn = get_mnist_network(model_path=model_path, softmax=softmax)
 
-
-    # these contraints encapsulate that the different values for each digit are mutually exclusive
-    # i.e. the digit *must* take a value (0-9) and if any value is true then no other value can be true
-    constraints_x = ((~x0 | ~x1) & (~x0 | ~x2) & (~x0 | ~x3) & (~x0 | ~x4) & (~x0 | ~x5) & (~x0 | ~x6) & (~x0 | ~x7) & (~x0 | ~x8) & (~x0 | ~x9)
-                    & (~x1 | ~x2) & (~x1 | ~x3) & (~x1 | ~x4) & (~x1 | ~x5) & (~x1 | ~x6) & (~x1 | ~x7) & (~x1 | ~x8) & (~x1 | ~x9)
-                    & (~x2 | ~x3) & (~x2 | ~x4) & (~x2 | ~x5) & (~x2 | ~x6) & (~x2 | ~x7) & (~x2 | ~x8) & (~x2 | ~x9)
-                    & (~x3 | ~x4) & (~x3 | ~x5) & (~x3 | ~x6) & (~x3 | ~x7) & (~x3 | ~x8) & (~x3 | ~x9)
-                    & (~x4 | ~x5) & (~x4 | ~x6) & (~x4 | ~x7) & (~x4 | ~x8) & (~x4 | ~x9)
-                    & (~x5 | ~x6) & (~x5 | ~x7) & (~x5 | ~x8) & (~x5 | ~x9)
-                    & (~x6 | ~x7) & (~x6 | ~x8) & (~x6 | ~x9)
-                    & (~x7 | ~x8) & (~x7 | ~x9)
-                    & (~x8 | ~x9)
-                    & (x0 | x1 | x2 | x3 | x4 | x5 | x6 | x7 | x8 | x9))
-    # fmt: on
-
-    sum6 = x6 & constraints_x
-    sum6.ref()
-
-    return sum6
-
-
-class NetworkPlusCircuitMinimal(nn.Module):
-    def __init__(
-        self, network: torch.nn.Module, circuit: SddNode, parse_to_native: bool
-    ):
-        super().__init__()
-        self.network = network
-        self.softmax = nn.Softmax(dim=-1)
-        self.circuit = circuit if not parse_to_native else sdd_parse_native(circuit)
-
-    def forward(self, x):
-        digit1_output = self.network(x)
-        activation1 = self.softmax(digit1_output)
-        sdd_output = eval_sdd(
-            self.circuit, operator.add, operator.mul, 0, 1, activation1
+    # for each sum, get a network+circuit module
+    # these will be used both for inference and for bound propagation
+    net_and_circuit_per_sum = {
+        sum_: NetworksPlusCircuit(
+            networks=[mnist_cnn] * num_digits,
+            circuit=sdd_,
+            softmax_net_outputs=[not softmax] * num_digits,
+            parse_to_native=True,
         )
+        for sum_, sdd_ in test_dataset.sdd_per_sum.items()
+    }
 
-        return sdd_output
+    # get the dataset examples that were classified correctly
+    # only there we perform verification for now
+    results_path = Path(__file__).parent.parent / "mnist_addition/results"
+    correctly_classified_idxs = get_correctly_classified_examples(
+        test_dataset, net_and_circuit_per_sum, results_path, softmax, num_digits
+    )
+
+    # let auto-LiRPA know I want to use the custom operators for bounding
+    # softmax and concatenation
+    register_custom_op("onnx::Softmax", CustomBoundSoftmax)
+    register_custom_op("onnx::Concat", CustomConcat)
+
+    # construct bounded module for each of the 19 network+circuit graphs
+    bounded_module_per_sum = {
+        sum_: BoundedModule(
+            net_plus_circuit,
+            torch.empty_like(test_dataset[0][0]),
+            verbose=False,
+        )
+        for sum_, net_plus_circuit in net_and_circuit_per_sum.items()
+    }
+
+    return bounded_module_per_sum, correctly_classified_idxs
 
 
 if __name__ == "__main__":
-    # get trained CNN
-    model_path = Path(__file__).parents[1] / "mnist_addition/checkpoints/trained_model.pth"
-    mnist_cnn = get_mnist_network(model_path=model_path)
+    # declare global variables
+    softmax = True
+    num_digits = 2
+    epsilon = 0.001
 
-    # data stuff
-    transform = transforms.Compose(
-        [
-            transforms.ToTensor(),
-            transforms.Normalize((0.5,), (0.5,)),
-        ]
-    )
-    test_dataset = MNIST(root="data/", train=True, download=True, transform=transform)
-    test_dl = DataLoader(test_dataset, batch_size=32, shuffle=True)
-    test_input = torch.unsqueeze(test_dataset[13][0], 0)
+    # get the dataset for this number of digits
+    test_dataset = MultiDigitAdditionDataset(train=False, num_digits=num_digits)
 
-    # create the network+circuit nn.Module to pass to auto-LiRPA for bound computation
-    test_nesy = NetworkPlusCircuitMinimal(
-        network=mnist_cnn,
-        circuit=get_test_circuit(),
-        parse_to_native=True,
+    # get a bounded version of the network+circuit structre for each sum
+    # also get the indices that were classified correctly and so should be verified
+    (
+        bounded_module_per_sum,
+        correctly_classified_idxs,
+    ) = get_bounded_modules_and_samples_to_verify(
+        softmax,
+        num_digits,
+        test_dataset,
     )
 
-    # let auto-LiRPA know I want to use BoundSoftmax
-    from nesy_veri.custom_softmax import CustomBoundSoftmax
-    register_custom_op("onnx::Softmax", CustomBoundSoftmax)
+    num_samples_checked = 0
+    num_samples_robust = 0
 
-    # construct bounded module for the network+circuit graph
-    lirpa_model = BoundedModule(test_nesy, torch.empty_like(test_input), verbose=False)
+    # go through the samples to verify
+    for idx in track(correctly_classified_idxs):
+        input_imgs, sum_label = test_dataset[idx]
 
-    # check what happens for several epsilons
-    for epsilon in [0.0001, 0.0005, 0.001, 0.005, 0.01, 0.05, 0.1]:
         # create perturbed input
+        # maybe the first line can be outside the loop?
         ptb = PerturbationLpNorm(norm=np.inf, eps=epsilon)
-        my_input = BoundedTensor(test_input, ptb)
+        ptb_input = BoundedTensor(input_imgs, ptb)
 
-        # compute bounds for the perturbed test_input
-        lb, ub = lirpa_model.compute_bounds(x=my_input, method="IBP")
+        # perform bound propagation for each sum
+        # each sum has its own network+circuit architecture
+        # so we will get a lower/upper bound for each of the 19 sums
+        bounds_per_sum = {
+            sum_: [
+                bound.item()
+                for bound in bounded_module.compute_bounds(
+                    x=ptb_input, method="IBP"
+                )
+            ]
+            for sum_, bounded_module in bounded_module_per_sum.items()
+        }
 
-        print(
-            f"Epsilon: {epsilon}\t\t pred = {test_nesy(test_input).item():.3f},  lb = {lb.item():.3f},  ub = {ub.item():.3f}"
-        )
+        num_samples_checked += 1
+        num_samples_robust += example_is_robust(bounds_per_sum, sum_label)
+
+    print(
+        f"Epsilon: {epsilon:<15}",
+        f"#total: {len(test_dataset)}, \t ",
+        f"#correct: {len(correctly_classified_idxs)}, {round(((len(correctly_classified_idxs) / len(test_dataset))*100), 2)}% \t ",
+        f"#robust correct: {num_samples_robust}, {round(((num_samples_robust / len(test_dataset))*100), 2)}% ",
+    )
