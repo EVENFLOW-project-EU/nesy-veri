3b54db1c16304c36ce0ce6a93a7eb35724ac393f
yuzer <bmanginas@gmail.com>
2024-11-26 11:22:25 +0200

    port MNIST addition to Marabou, it crashes from out-of-memory in the first example :)

---
+++ nesy_veri/examples/mnist_addition/marabou/custom_marabou/custom_read_onnx.py
@@ -0,0 +1,14 @@
+from nesy_veri.examples.mnist_addition.marabou.custom_marabou.custom_marabou_network import CustomMarabouNetworkONNX
+
+def custom_read_onnx(filename, inputNames=None, outputNames=None):
+    """Constructs a MarabouNetworkONNX object from an ONNX file
+
+    Args:
+        filename (str): Path to the ONNX file
+        inputNames (list of str, optional): List of node names corresponding to inputs
+        outputNames (list of str, optional): List of node names corresponding to outputs
+
+    Returns:
+        :class:`~maraboupy.MarabouNetworkONNX.MarabouNetworkONNX`
+    """
+    return CustomMarabouNetworkONNX(filename, inputNames, outputNames)
