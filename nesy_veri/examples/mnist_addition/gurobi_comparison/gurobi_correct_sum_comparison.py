86380603e6b20dff3cf10455842622e5e6982dd2
Vasilis Manginas <bmanginas@gmail.com>
2025-01-06 23:23:03 +0000

    add logging for Gurobi comparison, also add nn bound propagation time in the reported Gurobi time

--- nesy_veri/examples/mnist_addition/gurobi_comparison/gurobi_correct_sum_comparison.py
+++ nesy_veri/examples/mnist_addition/gurobi_comparison/gurobi_correct_sum_comparison.py
@@ -1,4 +1,8 @@
+import os
+import json
 import torch
+import random
+import argparse
 import statistics
 import numpy as np
 import gurobipy as gp
@@ -32,12 +36,16 @@ def get_bounds_for_sum_gurobi(
 
     images_bounds = {}
     ptb = PerturbationLpNorm(norm=np.inf, eps=epsilon)
+    nn_veri_time = 0
     for d in range(num_digits):
         # get perturbed input for this image
         ptb_input = BoundedTensor(input_imgs[d].unsqueeze(0), ptb)
 
         # compute lower/upper bounds for this image
+        start = time()
         lb, ub = bounded_network.compute_bounds(x=(ptb_input,), method="IBP")
+        end = time()
+        nn_veri_time += end - start
 
         # append this image bounds to a dict to be used by the SDD -> Gurobi translation
         img_bounds = {
@@ -51,6 +59,7 @@ def get_bounds_for_sum_gurobi(
     #         print()
 
     # calculate the minimum and maximum probability for the correct sum
+    start = time()
     model, var = sdd_to_gurobi_model(
         correct_sum_sdd,
         bounds=images_bounds,
@@ -58,7 +67,9 @@ def get_bounds_for_sum_gurobi(
             list(range(i * 10 + 1, (i + 1) * 10 + 1)) for i in range(num_digits)
         ],
     )
+    construction_time = time() - start
 
+    start = time()
     # find minimum
     model.setObjective(var, sense=gp.GRB.MINIMIZE)
     model.optimize()
@@ -68,17 +79,33 @@ def get_bounds_for_sum_gurobi(
     model.setObjective(var, sense=gp.GRB.MAXIMIZE)
     model.optimize()
     maximum = model.ObjVal
+    end = time()
+    solve_time = end - start
 
-    return minimum, maximum
+    model.close()
+
+    return minimum, maximum, nn_veri_time + solve_time, construction_time
 
 
-if __name__ == "__main__":
+def verify_experiment(digits: list[int], num_epochs: int, epsilon: float):
+    print(f"Epsilon: {epsilon}")
+    print(f"Digits: {digits}")
+    print(f"Number of epochs: {num_epochs}")
+
+    log_file = f"results_{epsilon}_{digits}_{num_epochs}"
+
+    if os.path.exists(f"results/{log_file}.json"):
+        raise RuntimeError("log file exists, bye bye!")
+
+    torch.set_num_threads(1)
+    # gp.setParam("Threads", 1)
+
     # get trained CNN
     model_path = (
         Path(__file__).parent.parent
-        / "checkpoints/model_checkpoints/trained_model_1_epoch_softmax.pth"
+        / f"checkpoints/model_checkpoints/trained_model_{num_epochs}_epochs_softmax.pth"
     )
-    mnist_cnn = get_mnist_network(model_path=model_path, num_epochs=1)
+    mnist_cnn = get_mnist_network(model_path=model_path, num_epochs=num_epochs)
 
     # TODO: DOES THIS NEED TO BE REPEATED?
     # let auto-LiRPA know I want to use the custom operators for bounding
@@ -86,14 +113,16 @@ if __name__ == "__main__":
     register_custom_op("onnx::Softmax", CustomBoundSoftmax)
     register_custom_op("onnx::Concat", CustomConcat)
 
+    data = {}
+
+    def log():
+        with open(f"results/{log_file}.json", "w") as f:
+            json.dump(data, f, indent=2)
+
     # declare number of MNIST digits for this experiment
-    for num_digits in [2, 3, 4, 5]:
-        start = time()
+    for num_digits in digits:
+        data[num_digits] = {}
         test_dataset = MultiDigitAdditionDataset(train=False, num_digits=num_digits)
-        end = time()
-        print(
-            f"{num_digits}-digit SDD generation took {end-start:.4f} seconds, #sums = {len(test_dataset.sdd_per_sum.keys())}"
-        )
 
         # for each sum, get a network+circuit module
         # these will be used both for inference and for bound propagation
@@ -102,28 +131,33 @@ if __name__ == "__main__":
             sum_: NetworksPlusCircuit(
                 networks=[mnist_cnn] * num_digits,
                 circuit=sdd_,
-                categorical_idxs=[x + 1 for x in range(num_digits*10)],
+                categorical_idxs=[x + 1 for x in range(num_digits * 10)],
                 parse_to_native=True,
             )
             for sum_, sdd_ in test_dataset.sdd_per_sum.items()
         }
         end = time()
-        print(
-            f"Constructing the Network+Circuit for all sums took {(end-start):.4f} seconds"
-        )
+        data[num_digits]["Network+Circuit construction time"] = end - start
+        log()
+
+        data[num_digits]["#nodes per sum"] = {
+            sum_: sdd_.size() for sum_, sdd_ in test_dataset.sdd_per_sum.items()
+        }
+        log()
 
         # construct bounded module for each of the network+circuit graphs
-        start = time()
-        bounded_module_per_sum = {
-            sum_: BoundedModule(
+        bounded_module_per_sum = {}
+        data[num_digits]["Bounded Module construction time"] = {}
+        for sum_, net_plus_circuit in net_and_circuit_per_sum.items():
+            start = time()
+            bounded_module_per_sum[sum_] = BoundedModule(
                 net_plus_circuit,
                 torch.empty_like(test_dataset[0][0]),
                 verbose=False,
             )
-            for sum_, net_plus_circuit in net_and_circuit_per_sum.items()
-        }
-        end = time()
-        print(f"Getting the BoundedModule for all sums took {(end-start):.4f} seconds")
+            end = time()
+            data[num_digits]["Bounded Module construction time"][sum_] = end - start
+            log()
 
         # get bounded NN
         start = time()
@@ -133,9 +167,9 @@ if __name__ == "__main__":
             verbose=False,
         )
         end = time()
-        print(f"Getting the BoundedModule for the CNN took {(end-start):.4f} seconds")
+        data[num_digits]["Bounded CNN construction time"] = end - start
+        log()
 
-        epsilon = 0.001
         timings_e2e = []
         timings_grb = []
         num_samples_robust_e2e = 0
@@ -144,7 +178,13 @@ if __name__ == "__main__":
         lower_bounds_grb, upper_bounds_grb = [], []
         grb_problematic_samples = 0
 
-        for input_imgs, sum_label in track(test_dataset):
+        data[num_digits]["E2E"] = {}
+        data[num_digits]["GRB"] = {}
+        data[num_digits]["#problematic samples"] = 0
+
+        for i, (input_imgs, sum_label) in track(
+            enumerate(test_dataset), total=len(test_dataset)
+        ):
             # create perturbed input
             ptb = PerturbationLpNorm(norm=np.inf, eps=epsilon)
             ptb_input = BoundedTensor(input_imgs, ptb)
@@ -160,23 +200,40 @@ if __name__ == "__main__":
 
             try:
                 # get bounds per sum for the Gurobi thing
-                grb_start = time()
-                correct_sum_min, correct_sum_max = get_bounds_for_sum_gurobi(
-                    bounded_network=bounded_cnn,
-                    input_imgs=input_imgs,
-                    epsilon=epsilon,
-                    correct_sum_sdd=test_dataset.sdd_per_sum[sum_label],
-                    num_digits=num_digits,
+                correct_sum_min, correct_sum_max, solve_time, construction_time = (
+                    get_bounds_for_sum_gurobi(
+                        bounded_network=bounded_cnn,
+                        input_imgs=input_imgs,
+                        epsilon=epsilon,
+                        correct_sum_sdd=test_dataset.sdd_per_sum[sum_label],
+                        num_digits=num_digits,
+                    )
                 )
-                grb_end = time()
+
+                data[num_digits]["E2E"][i] = {
+                    "sum_label": sum_label,
+                    "runtime": e2e_end - e2e_start,
+                    "lower_bound": max(correct_sum_lb, 0),
+                    "upper_bound": min(correct_sum_ub, 1),
+                    "robust": correct_sum_lb > 0.5,
+                }
+                data[num_digits]["GRB"][i] = {
+                    "sum_label": sum_label,
+                    "runtime": solve_time,
+                    "construction_time": construction_time,
+                    "lower_bound": correct_sum_min,
+                    "upper_bound": correct_sum_max,
+                    "robust": correct_sum_min > 0.5,
+                }
+                log()
 
                 # update timing info
                 timings_e2e.append(e2e_end - e2e_start)
-                timings_grb.append(grb_end - grb_start)
+                timings_grb.append(solve_time)
 
                 # update robustness lists
-                num_samples_robust_e2e += (correct_sum_lb > 0.5)
-                num_samples_robust_grb += (correct_sum_min > 0.5)
+                num_samples_robust_e2e += correct_sum_lb > 0.5
+                num_samples_robust_grb += correct_sum_min > 0.5
 
                 # update bound lists
                 lower_bounds_e2e.append(correct_sum_lb)
@@ -186,6 +243,8 @@ if __name__ == "__main__":
 
             except Exception as e:
                 grb_problematic_samples += 1
+                data[num_digits]["#problematic samples"] += 1
+                log()
 
         print(
             f"E2E - ",
@@ -209,3 +268,40 @@ if __name__ == "__main__":
             f"robustness(GRB): {round(((num_samples_robust_grb / len(test_dataset))*100), 2)}%   ",
             f"({grb_problematic_samples} problematic samples)",
         )
+
+
+if __name__ == "__main__":
+    # the way to call this is:
+
+    # python nesy_veri/examples/mnist_addition/gurobi_comparison/gurobi_correct_sum_comparison.py --digits 2 3 4 5 --num_epochs 10 --epsilon 0.01
+
+    parser = argparse.ArgumentParser(description="Verify experiment settings.")
+
+    parser.add_argument(
+        "--digits",
+        type=int,
+        nargs="+",
+        required=True,
+        help="List of digits to verify (e.g., 1 2 3).",
+    )
+    parser.add_argument(
+        "--num_epochs",
+        type=int,
+        required=True,
+        help="Number of epochs for the experiment.",
+    )
+    parser.add_argument(
+        "--epsilon",
+        type=float,
+        required=True,
+        help="Tolerance value (epsilon).",
+    )
+
+    args = parser.parse_args()
+
+    # Call the function with parsed arguments
+    verify_experiment(
+        digits=args.digits,
+        num_epochs=args.num_epochs,
+        epsilon=args.epsilon,
+    )
